f8db2b6f04135d3493e025d84413634b7028d061
Rick Henry <fredericmhenry@gmail.com>
2019-01-06 19:23:45 -0500

    add tag model and list tags endpoint
    Signed-off-by: Rick Henry <fredericmhenry@gmail.com>

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
@@ -38,3 +39,13 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Tag(models.Model):
+    """Tag to be use for a recipe"""
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
